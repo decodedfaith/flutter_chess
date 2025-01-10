9f412a3c57794261d20f7e63e7e9c500e9f59b2c
decodedfaith <komolafefaith@gmail.com>
2025-01-10 21:33:05 +0100

    -fixed the getValidMoves forthe king

--- lib/game/pieces/king.dart
+++ lib/game/pieces/king.dart
@@ -3,10 +3,16 @@
 import 'package:flutter_chess/game/chess_board.dart';
 import 'package:flutter_chess/game/chess_piece.dart';
 import 'package:flutter_chess/game/position.dart';
+import 'package:flutter_chess/models/player_color.dart';
 
 class King extends ChessPiece {
-  King(PieceColor color, Position position) : super(color, 'king', position);
+  King(PlayerColor color, Position position) : super(color, 'king', position);
   
+  @override
+  King copyWith({Position? position}) {
+    return King(color, position ?? this.position);
+  }
+
   @override
   String getSvgAssetPath() {
     return 'assets/chess_pieces_svg/${color.name}-king.svg';
@@ -27,8 +33,27 @@ class King extends ChessPiece {
     return false;
   }
   
-  getValidMoves(){
-    return;
+  @override
+  List<Position> getValidMoves(ChessBoard board) {
+    List<Position> moves = [];
+    List<List<int>> offsets = [
+      [1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]
+    ];
+
+    for (var offset in offsets) {
+      int newRow = position.row + offset[0];
+      int newCol = position.col + offset[1];
+      Position move = Position(row: newRow, col: newCol);
+
+      if (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+        if (board.isEmpty(move) || board.getPiece(move)?.color != color) {
+          moves.add(move);
+        }
+      }
+    }
+
+    return moves.where((move) => board.isValidMove(position, move, this)).toList();
   }
+
 
 }
