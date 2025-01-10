fffb67ba7d6a18f7c38eca06343b550a42815861
decodedfaith <komolafefaith@gmail.com>
2025-01-10 21:36:00 +0100

    -refactored out the enum class out of chess_piece.dart

--- lib/game/chess_piece.dart
+++ lib/game/chess_piece.dart
@@ -1,23 +1,21 @@
 import 'package:flutter_chess/game/chess_board.dart';
 import 'package:flutter_chess/game/position.dart';
-
-enum PieceColor {
-  white,
-  black;
-
-  // Method to return the enum name as a string
-  String get name => toString().split('.').last;
-}
+import 'package:flutter_chess/models/player_color.dart';
 
 abstract class ChessPiece {
-  final PieceColor color;
+  final PlayerColor color;
   Position position;
   final String type;
 
   ChessPiece(this.color, this.type, this.position);
 
+  ChessPiece copyWith({Position? position});
+
+
   // Abstract method to get SVG asset path
   String getSvgAssetPath();
 
   bool isValidMove(Position toPosition, ChessBoard board);
+
+  List<Position> getValidMoves(ChessBoard board);
 }
