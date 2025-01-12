a51775f634b42a98b6f7bf187fd8b3d78fabb946
decodedfaith <komolafefaith@gmail.com>
2025-01-12 23:57:00 +0100

    -Little fix on chessgame screen

--- lib/screens/chess_game_screen.dart
+++ lib/screens/chess_game_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chess/blocs/chess_cubit.dart';
 import 'package:flutter_chess/game/chess_board.dart';
 import 'package:flutter_chess/screens/chess_board_widget.dart';
 
 class ChessGameScreen extends StatelessWidget {
   final ChessBoard chessBoard = ChessBoard();
+  final ChessCubit chessCubit;
 
-  ChessGameScreen({super.key}) {
+  ChessGameScreen({super.key, required this.chessCubit}) {
     chessBoard.initializeBoard(); // Initialize the board with pieces
   }
 
@@ -15,7 +17,7 @@ class ChessGameScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Chess Game'),
       ),
-      body: ChessBoardWidget(chessBoard: chessBoard),
+      body: ChessBoardWidget(chessBoard: chessBoard, chessCubit: chessCubit,),
     );
   }
 }
