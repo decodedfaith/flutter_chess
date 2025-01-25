3a0575134fdc66435acfb6a41c4962198cd651fa
decodedfaith <komolafefaith@gmail.com>
2025-01-25 22:46:46 +0100

    -Updated the Position model class

--- lib/game/position.dart
+++ lib/game/position.dart
@@ -1,16 +1,16 @@
 // lib/game/chess_piece.dart
 
 class Position {
-  final int row;  // Corresponds to rows (1 to 8, where row 0 is the 1st row on the board)
-  final int col;  // Corresponds to columns (0 to 7, where column 0 is 'a', column 7 is 'h')
+  final String col;  // Corresponds to columns (a to h, where column a is the first column on the board)
+  final int row;  // Corresponds to rows (1 to 8, where row 1 is the 1st row on the board)
 
   Position({required this.row, required this.col});
 
   // Convert the column index to a letter for better visualization (e.g., 0 -> 'a', 1 -> 'b')
-  String get colLetter => String.fromCharCode(col + 97);
+  // String get colLetter => String.fromCharCode(col + 97);
 
   // Convert the position to the standard chess notation (e.g., 0, 0 -> 'a1')
-  String get notation => '$colLetter${8 - row}';
+  String get notation => '$col$row';
 
   @override
   bool operator ==(Object other) =>
@@ -27,7 +27,7 @@ class Position {
   factory Position.fromNotation(String notation) {
     int col = notation.codeUnitAt(0) - 97;  // Convert 'a' to 0, 'b' to 1, etc.
     int row = 8 - int.parse(notation[1]);  // Convert '1' to 7, '8' to 0, etc.
-    return Position(row: row, col: col);
+    return Position(row: row, col: String.fromCharCode('a'.codeUnitAt(0) + col));
   }
 }
 
